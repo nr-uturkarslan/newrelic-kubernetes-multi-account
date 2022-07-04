b15ceea2c8831007c1db7a4a7ae1da62f211396c
uturkarslan <ugurturkarslan@gmail.com>
2022-07-04 20:34:05 +0200

    Add Terraform scripts

---
+++ infra/terraform/01_platform/01_variables.tf
@@ -0,0 +1,75 @@
+### Variables ###
+
+## General
+
+# project
+variable "project" {
+  type    = string
+  default = "nr"
+}
+
+# location_long
+variable "location_long" {
+  type    = string
+  default = "westeurope"
+}
+
+# location_short
+variable "location_short" {
+  type    = string
+  default = "euw"
+}
+
+# stage_long
+variable "stage_long" {
+  type    = string
+  default = "dev"
+}
+
+# stage_short
+variable "stage_short" {
+  type    = string
+  default = "d"
+}
+
+# instance
+variable "instance" {
+  type    = string
+  default = "001"
+}
+
+## Specific
+
+# shared
+variable "shared" {
+  type    = string
+  default = "shared"
+}
+
+# platform
+variable "platform" {
+  type    = string
+  default = "platform"
+}
+
+# stats
+variable "stats" {
+  type    = string
+  default = "stats"
+}
+
+## Resource Names
+
+# Resource Group
+variable "project_resource_group_name" {
+  type = string
+}
+
+# Kubernetes Cluster
+variable "project_kubernetes_cluster_name" {
+  type = string
+}
+
+variable "project_kubernetes_cluster_nodepool_name" {
+  type = string
+}
