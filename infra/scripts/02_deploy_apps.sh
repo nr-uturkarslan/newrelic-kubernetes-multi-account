59724ebde3bd5dc3f6bd723c6403e8d1e82ff398
uturkarslan <ugurturkarslan@gmail.com>
2022-07-04 20:08:20 +0200

    Improve

--- infra/scripts/02_deploy_apps.sh
+++ infra/scripts/02_deploy_apps.sh
@@ -6,13 +6,29 @@
 
 ### Set variables
 
+bashLoggerName="bashlogger"
+
 clusterName="kind-test-cluster-001"
 team1="team1"
 team2="team2"
 
-# helm dependency build ../charts/nri-prometheus
+####################
+### Build & Push ###
+####################
 
-# Team 1
+# Bash Logger
+docker build \
+  --tag $bashLoggerName \
+  ../apps/bashlogger
+docker push "${DOCKERHUB_NAME}/${bashLoggerName}"
+#########
+
+##############
+### TEAM 1 ###
+##############
+
+### New Relic Prometheus ###
+helm dependency build "../charts/nri-prometheus"
 helm upgrade nri-prometheus \
   --install \
   --wait \
@@ -21,15 +37,28 @@ helm upgrade nri-prometheus \
   --namespace $team1 \
   --set global.cluster=$clusterName \
   --set licenseKey=$NEWRELIC_LICENSE_KEY \
-  ../charts/nri-prometheus
+  "../charts/nri-prometheus"
 
-# Team 2
-helm upgrade nri-prometheus \
+### New Relic Logging ###
+helm dependency build "../charts/nri-logging"
+helm upgrade nri-logging \
   --install \
   --wait \
   --debug \
   --create-namespace \
-  --namespace $team2 \
+  --namespace $team1 \
   --set global.cluster=$clusterName \
   --set licenseKey=$NEWRELIC_LICENSE_KEY \
-  ../charts/nri-prometheus
+  "../charts/nri-logging"
+
+### Bashlogger ###
+helm upgrade $bashLoggerName \
+  --install \
+  --wait \
+  --debug \
+  --create-namespace \
+  --namespace $team1 \
+  --set dockerhubName=$DOCKERHUB_NAME \
+  --set name=$bashLoggerName \
+  "../charts/$bashLoggerName"
+#########
