344c4a948b2072560a28d748325b866320210917
uturkarslan <ugurturkarslan@gmail.com>
2022-07-15 15:07:56 +0300

    Add limiting

--- teams-on-shared-nodes/02_bravo_team/apps/bravo-persistancy-service/services/list/list_handler.go
+++ teams-on-shared-nodes/02_bravo_team/apps/bravo-persistancy-service/services/list/list_handler.go
@@ -2,6 +2,7 @@ package list
 
 import (
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 	"github.com/rs/zerolog"
@@ -24,8 +25,13 @@ func (handler ListHandler) Run(
 	// Log start of method execution
 	commons.LogWithContext(ginctx, zerolog.InfoLevel, "Create method is triggered...")
 
+	// Parse request param
+	limit, err := handler.parseRequestParam(ginctx)
+	if err != nil {
+		return
+	}
 	// Retrieve all values DB
-	values, err := handler.DbClient.FindAll(ginctx)
+	values, err := handler.DbClient.FindAll(ginctx, limit)
 	if err != nil {
 		commons.CreateFailedHttpResponse(ginctx, http.StatusInternalServerError,
 			"Entity could not be saved into the DB.")
@@ -37,6 +43,40 @@ func (handler ListHandler) Run(
 
 	// Log end of method execution
 	commons.LogWithContext(ginctx, zerolog.InfoLevel, "List method is executed.")
+}
+
+func (ListHandler) parseRequestParam(
+	ginctx *gin.Context,
+) (
+	*int64,
+	error,
+) {
+
+	// Parse request param
+	limitAsString := ginctx.Query("limit")
+
+	var limit *int64
+	if limitAsString == "" {
+		// Nothing specific declared -> Set to nil
+		limit = nil
+
+		// Log provided limit
+		commons.LogWithContext(ginctx, zerolog.InfoLevel, "No limit provided")
+	} else {
+		// Specific limit declared -> Parse
+		limitAsInt, err := strconv.ParseInt(limitAsString, 10, 64)
+		if err != nil {
+			commons.CreateFailedHttpResponse(ginctx, http.StatusInternalServerError,
+				"Given limit parameter is not valid.")
+			return nil, err
+		}
+		limit = &limitAsInt
+
+		// Log provided limit
+		commons.LogWithContext(ginctx, zerolog.InfoLevel, "Limit provided: "+limitAsString)
+	}
+
+	return limit, nil
 }
 
 func (ListHandler) createResponseDto(
